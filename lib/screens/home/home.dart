89ba48c9d1189b100cd2b9d59cf8eb7357538cd0
mhmdhkl <mhmdhklhkmh@gmail.com>
2025-06-13 20:31:49 +0700

    Pengaturan dasar Home screen dengan AppBar dan struktur daftar kategori

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tugasbesar_berita/common/colors.dart';
+import 'package:tugasbesar_berita/screens/home/widgets/CategoryItem.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,11 +10,70 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<String> categories = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology',
+  ];
+
+  int activeCategory = 0;
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: const Text('News App')),
-      body: const Center(child: Text('Home Screen')),
+      appBar: AppBar(
+        leadingWidth: 100,
+        leading: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Image.asset(
+              "assets/images/logo.png",
+              fit: BoxFit.contain,
+              color: AppColors.white,
+            ),
+          ),
+        ),
+        backgroundColor: AppColors.black,
+        elevation: 5,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.search, size: 34, color: AppColors.white),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 50,
+              width: size.width,
+              child: ListView.builder(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) => CategoryItem(
+                  index: index,
+                  categoryName: categories[index],
+                  activeCategory: activeCategory,
+                  onClick: () {
+                    setState(() {
+                      activeCategory = index;
+                    });
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Placeholder for news list
+          ],
+        ),
+      ),
     );
   }
 }
